c28e9fe5582d8fc7c291f1afa395e7cad9c6139b
choijinhap <wlsgkq123@gmail.com>
2020-06-26 22:46:37 +0900

    algorithm programmers lv1

--- PROGRAMMERS/LV1/k번째수.js
+++ PROGRAMMERS/LV1/k번째수.js
@@ -1,5 +1,7 @@
 function solution(array, commands) {
     var answer = [];
     answer=commands.map(item=>{return array.slice(item[0]-1,item[1]).sort((f,s)=>f>s)[item[2]-1]});
+    // answer=commands.map(([from,to,k])=>{return array.slice(from-1,to).sort((f,s)=>f>s)[k-1]});
+    
     return answer;
 }
