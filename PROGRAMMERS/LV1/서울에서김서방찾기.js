083c4c3cf48819b5a3f7d7842fd5eed885251475
choijinhap <wlsgkq123@gmail.com>
2020-07-16 01:55:51 +0900

    as

--- PROGRAMMERS/LV1/서울에서김서방찾기.js
+++ PROGRAMMERS/LV1/서울에서김서방찾기.js
@@ -1,15 +1,15 @@
 function solution(seoul) {
     let answer = '';
-    let count=-1;
-    answer=seoul.find(val=>{
+    let count = -1;
+    answer = seoul.find(val => {
         count++;
-        return val==='Kim'
+        return val === 'Kim'
     })
-    answer='김서방은 '+count +'에 있다';
+    answer = '김서방은 ' + count + '에 있다';
     return answer;
 }
 
 //better solution with indexOf
-function betterSolution(seoul){
-    return '김서방은 '+seoul.indexOf('Kim')+'에 있다';
+function betterSolution(seoul) {
+    return '김서방은 ' + seoul.indexOf('Kim') + '에 있다';
 }
