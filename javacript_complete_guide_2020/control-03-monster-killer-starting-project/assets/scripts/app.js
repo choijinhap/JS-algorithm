305b56e860fd33ff8d39141806eb94fdb4f081fb
choijinhap <wlsgkq123@gmail.com>
2020-09-20 18:47:35 +0900

    85 using if statements for checking the win-condition

--- javacript_complete_guide_2020/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ javacript_complete_guide_2020/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,13 +1,23 @@
 const ATTACK_VALUE = 10;
-
+const MONSTER_ATTACK_VALUE = 14;
 let chosenMaxLife = 100;
-let currentMonsterHealth= chosenMaxLife;
-let currentPlayerHealth=chosenMaxLife;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler(){
-    const damage = dealMonsterDamage(ATTACK_VALUE);
-    currentMonsterHealth-=damage;
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You won!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You lose!');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert('you have a draw!');
+  }
 }
-attackBtn.addEventListener('click',attackHandler);
+attackBtn.addEventListener('click', attackHandler);
