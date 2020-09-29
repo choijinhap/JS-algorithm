3b8f0f571eb70d125afbca5c39abbd2ffea54426
choijinhap <wlsgkq123@gmail.com>
2020-09-30 01:38:44 +0900

    로또번호생성기 최근번호제외 번호 추출 프로토타입 완성

--- javacript_complete_guide_2020/game/assets/scripts/app.js
+++ javacript_complete_guide_2020/game/assets/scripts/app.js
@@ -5,10 +5,10 @@ let recentDrwn = parseInt((nowDate - startDate) / WEEK_MS) + 1;
 const URL =
   'https://dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=' + recentDrwn;
 
-function getLastNums(){
-  const lastNums=[];
-  let xmlHttp=new XMLHttpRequest();
-  xmlHttp.open('get',URL,false);
+function getLastNums() {
+  const lastNums = [];
+  let xmlHttp = new XMLHttpRequest();
+  xmlHttp.open('get', URL, false);
   xmlHttp.send(null);
   console.log(JSON.parse(xmlHttp.responseText));
   lastNums.push(JSON.parse(xmlHttp.responseText).drwtNo1);
@@ -20,4 +20,32 @@ function getLastNums(){
   lastNums.push(JSON.parse(xmlHttp.responseText).bnusNo);
   return lastNums;
 }
-console.log(getLastNums());
+const lastNums = getLastNums();
+document.getElementById('lastNums').innerText = lastNums;
+
+function getRecmNums() {
+  const posNums = [];
+  const recmNums = [];
+  for (let i = 1; i < 46; i++) {
+    if (lastNums.indexOf(i) < 0) {
+      posNums.push(i);
+    }
+  }
+
+  let num;
+  for (let i = 0; i < 7; i++) {
+    num = Math.floor(Math.random() * posNums.length);
+    recmNums.push(posNums[num]);
+    const idx = posNums.indexOf(posNums[num]);
+      posNums.splice(idx, 1);
+      console.log(num);
+      console.log(posNums);
+    
+  }
+  return recmNums;
+}
+function printRecmNums() {
+  const recmNums = getRecmNums();
+  document.getElementById('recommend').innerText = recmNums;
+}
+document.getElementById('press').addEventListener('click', printRecmNums);
