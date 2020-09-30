65f3a95429e66b66815eb682d6bc14d552b10b86
choijinhap <wlsgkq123@gmail.com>
2020-09-30 19:05:38 +0900

    131 adding user choices to the game

--- javacript_complete_guide_2020/functions-01-starting-code/app.js
+++ javacript_complete_guide_2020/functions-01-starting-code/app.js
@@ -1,9 +1,27 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// const start = function () {
-//   console.log('Gamie is  starting...');
-// };
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_SELECTION = ROCK;
+let isGameRunning = false;
+function getPlayerChoice() {
+  const selection = prompt(
+    `select ${ROCK} ${PAPER} or ${SCISSORS}`
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_SELECTION} for you!`);
+    return DEFAULT_USER_SELECTION;
+  }
+  return selection;
+}
 
 startGameBtn.addEventListener('click', function () {
+  if (isGameRunning) {
+    return;
+  }
+  isGameRunning = true;
   console.log('Gamie is  starting...');
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
