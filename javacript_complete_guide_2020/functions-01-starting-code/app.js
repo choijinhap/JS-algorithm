26ddba4ec55e4068a5472d362c72993505b41e13
choijinhap <wlsgkq123@gmail.com>
2020-09-30 22:43:09 +0900

    138 function in function

--- javacript_complete_guide_2020/functions-01-starting-code/app.js
+++ javacript_complete_guide_2020/functions-01-starting-code/app.js
@@ -70,10 +70,13 @@ startGameBtn.addEventListener('click', function () {
 
 //not related to game
 const sumUp =  (...numbers)=>{
+    const validateNumber=(number)=>{
+        return isNaN(number)?0:number;
+    }
     let sum=0;
-    for(const num in numbers){
-        sum+=num;
+    for(const num of numbers){
+        sum+=validateNumber(num);
     }
     return sum;
 }
-console.log(sumUp(1,2,3,4));
+console.log(sumUp(1,2,'as',4));
