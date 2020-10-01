6ba701e94ba6962e86e4800bbb4002be79d2d9b7
choijinhap <wlsgkq123@gmail.com>
2020-10-01 17:08:40 +0900

    155 child parent element

--- javacript_complete_guide_2020/dom-01-starting-code/app.js
+++ javacript_complete_guide_2020/dom-01-starting-code/app.js
@@ -1,9 +1,21 @@
 const h1 = document.getElementById('main-title');
 console.log(h1.textContent);
-h1.textContent='asdfasdf';
+h1.textContent = 'asdfasdf';
 console.log(h1.textContent);
-h1.style.color='white';
-h1.style.backgroundColor='black'
+h1.style.color = 'white';
+h1.style.backgroundColor = 'black';
 
-const li=document.querySelector('li:last-of-type')
-li.textContent=li.textContent+' changed'
+const li = document.querySelector('li:last-of-type');
+li.textContent = li.textContent + ' changed';
+
+//child element
+const ul = document.querySelector('ul');
+ul.children;
+ul.children[0].textContent;
+ul.firstElementChild;
+//this is slow than upper
+ul.querySelector('li:first-of-type');
+
+
+
+console.log(ul.parentElement);
