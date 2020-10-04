2c9b21862e0da79b7dee1ad564e268c3ed365ddd
choijinhap <wlsgkq123@gmail.com>
2020-10-04 21:45:12 +0900

    203 array destructuring

--- javacript_complete_guide_2020/arrays-01-starting-code/app.js
+++ javacript_complete_guide_2020/arrays-01-starting-code/app.js
@@ -21,3 +21,23 @@ const droppedvalue = hobbies.shift();
 console.log(hobbies);
 console.log(poppedValue);
 console.log(droppedvalue);
+console.log(droppedvalue);
+
+hobbies.splice(1, 0, 'Good Food');
+console.log(hobbies);
+
+const removedElements = hobbies.splice(-2, 1);
+console.log(removedElements);
+
+const testArray = [1, 2, 3, 4, 5];
+
+const personData = [{ name: 'Max' }, { name: 'Manuel' }];
+const manuel = personData.find((value, idx, arr) => {
+  return value.name === 'Manuel';
+});
+manuel.name = 'Anna';
+console.log(manuel, personData);
+
+const nameData = ['Max', 'Schwarz', 'Mr', '30'];
+const [firstName, lastName, ...others] = nameData;
+console.log(firstName, lastName, others);
