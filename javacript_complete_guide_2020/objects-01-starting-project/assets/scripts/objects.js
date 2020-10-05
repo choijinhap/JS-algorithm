4600308b70dc06f46e3140f0dfbd7a944b0420c6
choijinhap <wlsgkq123@gmail.com>
2020-10-06 00:31:25 +0900

    220 demo app , property syntax

--- javacript_complete_guide_2020/objects-01-starting-project/assets/scripts/objects.js
+++ javacript_complete_guide_2020/objects-01-starting-project/assets/scripts/objects.js
@@ -1,26 +1,30 @@
-let person = {
-    //특수문자 띄어쓰기 가능
-  'first name': 'Max',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-  //함수
-  greet: function () {
-    alert('Hi there');
-  },
-  //숫자 0이상가능
-  1.5:'hello'
+const addMoiveBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+const movies = [];
+
+const addMovieHandler = () => {
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Math.random().toString(),
+  };
+  movies.push(newMovie);
+  console.log(movies);
 };
 
-//property 추가 ,변경
-person.isAdmin = true;
-person.age = 31;
-console.log(person);
-
-//property 제거
-delete person.age;
-//가능은하나 undefined를 직접 할당하는건 하지 않는게 좋다.
-person.age = undefined;
-console.log(person);
-
-console.log(person['first name']);
-console.log(person['1.5']);
+addMoiveBtn.addEventListener('click', addMovieHandler);
