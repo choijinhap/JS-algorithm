c095b802a8458d07757ce9f27def6d2b81a8c337
choijinhap <wlsgkq123@gmail.com>
2020-10-05 23:54:56 +0900

    218 property type

--- javacript_complete_guide_2020/objects-01-starting-project/assets/scripts/objects.js
+++ javacript_complete_guide_2020/objects-01-starting-project/assets/scripts/objects.js
@@ -1,10 +1,14 @@
 let person = {
+    //특수문자 띄어쓰기 가능
   'first name': 'Max',
   age: 30,
   hobbies: ['Sports', 'Cooking'],
+  //함수
   greet: function () {
     alert('Hi there');
   },
+  //숫자 0이상가능
+  1.5:'hello'
 };
 
 //property 추가 ,변경
@@ -19,3 +23,4 @@ person.age = undefined;
 console.log(person);
 
 console.log(person['first name']);
+console.log(person['1.5']);
