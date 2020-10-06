3f6b82537897cd4be3ab7f52fa1ea0e4c1f27174
choijinhap <wlsgkq123@gmail.com>
2020-10-06 20:50:36 +0900

    243 definig class , constructor method

--- javacript_complete_guide_2020/oop-01-starting-setup/assets/scripts/app.js
+++ javacript_complete_guide_2020/oop-01-starting-setup/assets/scripts/app.js
@@ -1,26 +1,40 @@
-const productList={
-    products:[
-        {
-            title:'Pillow',
-            imgUrl:'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcStvd1ZpnfLaj6rvXG7ujs3cl61uq5OktMG2Ynr8pVc4zalb-y9-ATrLvBXrP233g&usqp=CAc',
-            price:19.9,
-            description:'soft pillow'
-        },
-        {
-            title:'Carpet',
-            imgUrl:'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSbmOOcHqrHZTu8ZkMR3mPdN3igX3lPwcD-DKzOJzlhsN5u9xuxXh2HCCzC1iZW1nc_R_Hw58rFK4GmODiHvrNGdWMc91_kiJ3LRF0K-v29&usqp=CAc',
-            price:89.99,
-            description:'Good Carpet'
-        }
-    ],
-    render(){
-        const renderHook=document.getElementById('app')
-        const prodList=document.createElement('ul')
-        prodList.className='product-list'
-        for(const prod of this.products){
-            const prodEl=document.createElement('li');
-            prodEl.className='product-item'
-            prodEl.innerHTML=`
+class Product {
+  title = 'Default';
+  imgUrl;
+  description;
+  price;
+
+  constructor(title, imgUrl, description, price) {
+    this.title = title;
+    this.imgUrl = imgUrl;
+    this.description = description;
+    this.price = price;
+  }
+}
+
+const productList = {
+  products: [
+    new Product(
+      'Pillow',
+      'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcStvd1ZpnfLaj6rvXG7ujs3cl61uq5OktMG2Ynr8pVc4zalb-y9-ATrLvBXrP233g&usqp=CAc',
+      19.9,
+      'soft pillow'
+    ),
+    new Product(
+      'Carpet',
+      'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSbmOOcHqrHZTu8ZkMR3mPdN3igX3lPwcD-DKzOJzlhsN5u9xuxXh2HCCzC1iZW1nc_R_Hw58rFK4GmODiHvrNGdWMc91_kiJ3LRF0K-v29&usqp=CAc',
+      89.99,
+      'Good Carpet'
+    ),
+  ],
+  render() {
+    const renderHook = document.getElementById('app');
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
+    for (const prod of this.products) {
+      const prodEl = document.createElement('li');
+      prodEl.className = 'product-item';
+      prodEl.innerHTML = `
                 <div>
                     <img src=${prod.imgUrl} alt=${prod.title}/>
                     <div class="product-item__content">
@@ -30,10 +44,10 @@ const productList={
                         <button>Add to Cart</button>
                     </div>
                 </div>
-            `
-            prodList.append(prodEl);
-        }
-        renderHook.append(prodList);
+            `;
+      prodList.append(prodEl);
     }
-}
+    renderHook.append(prodList);
+  },
+};
 productList.render();
