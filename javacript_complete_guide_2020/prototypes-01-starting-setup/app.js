3b0565cb166c537d8278b9a38e86e0fbc7557ab0
choijinhap <wlsgkq123@gmail.com>
2020-10-08 18:11:06 +0900

    270 working with prototype

--- javacript_complete_guide_2020/prototypes-01-starting-setup/app.js
+++ javacript_complete_guide_2020/prototypes-01-starting-setup/app.js
@@ -1,10 +1,37 @@
+// class AgedPerson{
+//     printAge(){
+//         console.log(this.age)
+//     }
+// }
+
+// class Person extends AgedPerson{
+//     name='Max'
+//     constructor(){
+//         super();
+//         this.age=30;
+//     }
+//     greet(){
+//         console.log('hi')
+//     }
+// }
+
 function Person() {
   this.age = 30;
-  this.nmae = 'Max';
+  this.name = 'Max';
   this.greet = function () {
     console.log('hi!');
   };
 }
+// Person.prototype = {
+//   printAge() {
+//     console.log(this.age);
+//   },
+// };
+Person.prototype.printAge=function(){
+    console.log(this.age);
+}
 
-const person = new Person();
-person.greet();
+console.dir(Person);
+const p = new Person();
+p.greet();
+p.printAge();
