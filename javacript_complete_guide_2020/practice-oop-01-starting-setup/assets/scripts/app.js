197347d32b3bca7e28803e6135f1588c5c96dd3f
choijinhap <wlsgkq123@gmail.com>
2020-10-09 00:34:48 +0900

    280 creating project lists & parsing element data

---
+++ javacript_complete_guide_2020/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,16 @@
+class Tolltip {}
+class ProjectItem {}
+class ProjectList {
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    console.log(prjItems);
+  }
+}
+class App {
+  static init() {
+    const activeProjectList = new ProjectList('active');
+    const finishedProjectList = new ProjectList('finished');
+  }
+}
+
+App.init();
