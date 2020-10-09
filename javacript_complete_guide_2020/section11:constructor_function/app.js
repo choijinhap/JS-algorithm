9fae9587c602e27d67680aaf201645b34f2c9992
choijinhap <wlsgkq123@gmail.com>
2020-10-10 03:53:20 +0900

    s

--- javacript_complete_guide_2020/section11:constructor_function/app.js
+++ javacript_complete_guide_2020/section11:constructor_function/app.js
@@ -27,11 +27,14 @@ function Person() {
 //     console.log(this.age);
 //   },
 // };
-Person.prototype.printAge=function(){
-    console.log(this.age);
-}
+Person.prototype.printAge = function () {
+  console.log(this.age);
+};
 
 console.dir(Person);
 const p = new Person();
+const p2 = new Person();
 p.greet();
 p.printAge();
+console.dir(p.__proto__.__proto__);
+console.log(p.__proto__ === p2.__proto__);
