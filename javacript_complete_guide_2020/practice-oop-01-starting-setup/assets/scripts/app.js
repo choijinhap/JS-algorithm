3eb2dcf129ddfb95aac180fd1bad96f757969118
choijinhap <wlsgkq123@gmail.com>
2020-10-09 23:16:53 +0900

    281 switch project

--- javacript_complete_guide_2020/practice-oop-01-starting-setup/assets/scripts/app.js
+++ javacript_complete_guide_2020/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,9 +1,36 @@
 class Tolltip {}
-class ProjectItem {}
+class ProjectItem {
+  constructor(id, type) {
+    this.id = id;
+    this.type = type;
+    this.connectMoreInfoBtn();
+    this.connectSwitchBtn();
+  }
+  connectMoreInfoBtn() {}
+  connectSwitchBtn() {
+    const prjItemEl = document.getElementById(this.id);
+    const switchBtn = prjItemEl.querySelector('button:last-of-type');
+    //ProjectList에서 동작하는 함수가 등록되어야함
+    switchBtn.addEventListener('click');
+  }
+}
 class ProjectList {
+  projects = [];
   constructor(type) {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
-    console.log(prjItems);
+    for (const prjItem of prjItems) {
+      this.projects.push(
+        new ProjectItem(prjItem.id, type, this.updateProjectList)
+      );
+    }
+    console.log(this.projects);
+  }
+  addProject() {}
+  switchProject(projectId) {
+    // const prjIndex=this.projects.findIndex(p=>p.id===projectId);
+    // this.projects.splice(prjIndex,1);
+    //위코드와 동일함
+    this.projects = this.projects.filter((p) => p.id !== projectId);
   }
 }
 class App {
