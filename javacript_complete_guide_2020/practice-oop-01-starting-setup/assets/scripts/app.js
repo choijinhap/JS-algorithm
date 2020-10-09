e67ba6858f81ccac2039f8a574e8fc0b18e03f5f
choijinhap <wlsgkq123@gmail.com>
2020-10-09 23:35:47 +0900

    282 passing method references around

--- javacript_complete_guide_2020/practice-oop-01-starting-setup/assets/scripts/app.js
+++ javacript_complete_guide_2020/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,8 +1,9 @@
 class Tolltip {}
 class ProjectItem {
-  constructor(id, type) {
+  constructor(id, type, updateProjectList) {
     this.id = id;
     this.type = type;
+    this.updateProjectListHandler = updateProjectList;
     this.connectMoreInfoBtn();
     this.connectSwitchBtn();
   }
@@ -11,32 +12,46 @@ class ProjectItem {
     const prjItemEl = document.getElementById(this.id);
     const switchBtn = prjItemEl.querySelector('button:last-of-type');
     //ProjectList에서 동작하는 함수가 등록되어야함
-    switchBtn.addEventListener('click');
+    switchBtn.addEventListener('click', this.updateProjectListHandler);
   }
 }
 class ProjectList {
   projects = [];
   constructor(type) {
+    this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
       this.projects.push(
-        new ProjectItem(prjItem.id, type, this.updateProjectList)
+        new ProjectItem(prjItem.id, type, this.switchProject.bind(this))
       );
     }
     console.log(this.projects);
   }
-  addProject() {}
+  setSwitchHanderlFunction(switchHandlerFunction) {
+    this.switchHander = switchHandlerFunction;
+  }
+  addProject() {
+    console.log(this);
+  }
   switchProject(projectId) {
     // const prjIndex=this.projects.findIndex(p=>p.id===projectId);
     // this.projects.splice(prjIndex,1);
     //위코드와 동일함
     this.projects = this.projects.filter((p) => p.id !== projectId);
+    this.switchHander(this.projects.find((p) => p.id === projectId));
   }
 }
+
 class App {
   static init() {
     const activeProjectList = new ProjectList('active');
     const finishedProjectList = new ProjectList('finished');
+    activeProjectList.setSwitchHanderlFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHanderlFunction(
+      ProjectList.prototype.addProject.bind(activeProjectList)
+    );
   }
 }
 
