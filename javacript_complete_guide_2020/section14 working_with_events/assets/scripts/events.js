a8ca27b1c4eaeaf9afa3e1b1ad7f9302432aa751
choijinhap <wlsgkq123@gmail.com>
2020-10-15 21:43:47 +0900

    311 working with preventDefault()

--- javacript_complete_guide_2020/section14 working_with_events/assets/scripts/events.js
+++ javacript_complete_guide_2020/section14 working_with_events/assets/scripts/events.js
@@ -1,20 +1,32 @@
 const btn = document.querySelector('button');
 
-const btnClickHandler = () => {
+const btnClickHandler = (event) => {
   alert('button onclick');
+  console.log(event);
 };
 
-const anotherClickHandler=()=>{
-    console.log('another click')
-}
+const anotherClickHandler = () => {
+  console.log('another click');
+};
 
 // btn.onclick = btnClickHandler;
 // btn.onclick=anotherClickHandler;
 
 //for adding and removing bound function
-const boundFn=btnClickHandler.bind(this);
-btn.addEventListener('click',boundFn)
+const boundFn = btnClickHandler.bind(this);
+btn.addEventListener('click', boundFn);
 setTimeout(() => {
-    btn.removeEventListener('click',boundFn)    
+  btn.removeEventListener('click', boundFn);
 }, 2000);
 
+window.addEventListener('scroll', (event) => {
+  console.log(event);
+});
+
+const form = document.querySelector('form');
+
+//only form can have this
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log(event.target);
+});
