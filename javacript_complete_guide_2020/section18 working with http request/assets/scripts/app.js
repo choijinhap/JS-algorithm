49f023e9ed7c3466969cb205970d1eb06ee16033
choijinhap <wlsgkq123@gmail.com>
2020-10-18 18:48:43 +0900

    372 Promisfying Http Requests

--- javacript_complete_guide_2020/section18 working with http request/assets/scripts/app.js
+++ javacript_complete_guide_2020/section18 working with http request/assets/scripts/app.js
@@ -1,16 +1,45 @@
 const listEl = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
+function sendHttpRequest(method, url, data) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
+    xhr.send();
+  });
+  return promise;
+}
 
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-xhr.responseType = 'json';
-xhr.onload = function () {
-  const listOfPosts = xhr.response;
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector('h2').textContent = post.title.toUpperCase();
-    postEl.querySelector('p').textContent = post.body;
-    listEl.append(postEl);
-  }
-};
-xhr.send();
+// using async/await
+
+// async function fetchPosts() {
+//   const responseData = await sendHttpRequest(
+//     'GET',
+//     'https://jsonplaceholder.typicode.com/posts'
+//   );
+//   const listOfPosts = responseData;
+//   for (const post of listOfPosts) {
+//     const postEl = document.importNode(postTemplate.content, true);
+//     postEl.querySelector('h2').textContent = post.title.toUpperCase();
+//     postEl.querySelector('p').textContent = post.body;
+//     listEl.append(postEl);
+//   }
+// }
+
+function fetchPosts() {
+  sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
+    (responseData) => {
+      const listOfPosts = responseData;
+      for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listEl.append(postEl);
+      }
+    }
+  );
+}
+fetchPosts();
