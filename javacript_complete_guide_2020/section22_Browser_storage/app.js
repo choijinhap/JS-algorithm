4ab1d08a84efdbb221d91e898b5fdf2393c92ee9
choijinhap <wlsgkq123@gmail.com>
2020-10-21 01:34:52 +0900

    430 working with cookies

--- javacript_complete_guide_2020/section22_Browser_storage/app.js
+++ javacript_complete_guide_2020/section22_Browser_storage/app.js
@@ -7,18 +7,32 @@ const user = {
   age: 30,
 };
 
+// storeBtn.addEventListener('click', () => {
+//   sessionStorage.setItem('uid', userId);
+//   localStorage.setItem('user', JSON.stringify(user));
+// });
+
+// retrBtn.addEventListener('click', () => {
+//   const extractedId = sessionStorage.getItem('uid');
+//   const extractedUser = localStorage.getItem('user');
+//   console.log(JSON.parse(extractedUser));
+//   if (extractedId) {
+//     console.log('Got the id - ' + extractedId);
+//   } else {
+//     console.log('No id');
+//   }
+// });
+
 storeBtn.addEventListener('click', () => {
-  sessionStorage.setItem('uid', userId);
-  localStorage.setItem('user', JSON.stringify(user));
+  document.cookie = `uid=${userId}; max-age=5`;
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
 
 retrBtn.addEventListener('click', () => {
-  const extractedId = sessionStorage.getItem('uid');
-  const extractedUser = localStorage.getItem('user');
-  console.log(JSON.parse(extractedUser));
-  if (extractedId) {
-    console.log('Got the id - ' + extractedId);
-  } else {
-    console.log('No id');
-  }
+  const cookieData = document.cookie;
+  const data = cookieData.split(';').map((i) => i.trim());
+  const userValue = data[1].split('=');
+  console.log(userValue);
 });
+
+console.log(document.cookie);
