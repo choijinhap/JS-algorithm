7170142dbf03d7ca020417c637eda887a359f51f
choijinhap <wlsgkq123@gmail.com>
2020-10-20 21:52:28 +0900

    420 webpack for development & for production

--- javacript_complete_guide_2020/section20_modular/webpack.config.prod.js
+++ javacript_complete_guide_2020/section20_modular/webpack.config.prod.js
@@ -1,13 +1,15 @@
 const path = require('path')
 
 module.exports={
-    mode: 'development',
+    mode: 'production',
     entry:'./src/app.js',
     output:{
         filename: 'app.js',
         path:path.resolve(__dirname,'assets','scripts'),
         publicPath: 'assets/scripts/'
     },
-    //for debug
-    devtool:'cheap-module-eval-source-map'
+    //for production
+    devtool:'cheap-source-map'
+    
+
 };
