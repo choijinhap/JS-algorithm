eeab471c8748cc0f5fea1f5e50e12559a82d5170
choijinhap <wlsgkq123@gmail.com>
2020-10-21 00:11:47 +0900

    asd

--- javacript_complete_guide_2020/section20_modular/webpack.config.js
+++ javacript_complete_guide_2020/section20_modular/webpack.config.js
@@ -1,13 +1,15 @@
-const path = require('path')
+const path = require('path');
+const CleanPlugin = require('clean-webpack-plugin');
 
-module.exports={
-    mode: 'development',
-    entry:'./src/app.js',
-    output:{
-        filename: 'app.js',
-        path:path.resolve(__dirname,'assets','scripts'),
-        publicPath: 'assets/scripts/'
-    },
-    //for debug
-    devtool:'cheap-module-eval-source-map'
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'assets', 'scripts'),
+    publicPath: 'assets/scripts/',
+  },
+  //for debug source during develop
+  devtool: 'cheap-module-eval-source-map',
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
--- javacript_complete_guide_2020/section20_modular/webpack.config.prod.js
+++ javacript_complete_guide_2020/section20_modular/webpack.config.prod.js
@@ -1,15 +1,19 @@
 const path = require('path')
+const CleanPlugin=require('clean-webpack-plugin')
 
 module.exports={
     mode: 'production',
     entry:'./src/app.js',
     output:{
-        filename: 'app.js',
+        filename: '[contenthash].js',
         path:path.resolve(__dirname,'assets','scripts'),
         publicPath: 'assets/scripts/'
     },
     //for production
-    devtool:'cheap-source-map'
+    devtool:'cheap-source-map',
+    plugins:[
+        new CleanPlugin.CleanWebpackPlugin()
+    ]
     
 
 };
