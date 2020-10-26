cd56287c720e77e33ceb29345d54375a824cd992
choijinhap <wlsgkq123@gmail.com>
2020-10-26 22:49:54 +0900

    441 Trangspiling code

--- javacript_complete_guide_2020/section23_Browser_support/webpack.config.js
+++ javacript_complete_guide_2020/section23_Browser_support/webpack.config.js
@@ -7,13 +7,25 @@ module.exports = {
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, 'assets', 'scripts'),
-    publicPath: 'assets/scripts/'
+    publicPath: 'assets/scripts/',
   },
   devtool: 'cheap-module-eval-source-map',
   // devServer: {
   //   contentBase: './'
   // }
-  plugins: [
-    new CleanPlugin.CleanWebpackPlugin()
-  ]
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
