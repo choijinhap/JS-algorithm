2662c1e791a2d97bd146e10f3b530694850f6baa
choijinhap <wlsgkq123@gmail.com>
2020-10-27 00:29:51 +0900

    450 getting user location

---
+++ javacript_complete_guide_2020/section24_practice_placeApp/src/SharePlace.js
@@ -0,0 +1,38 @@
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+    console.log('a');
+    locateUserBtn.addEventListener('click', this.locateUserHandler.bind(this));
+    addressForm.addEventListener('submit', this.findAddressHandler.bind(this));
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert(
+        'Location feature is not unavailable in your browser - please use a more modern browser or manually enter an address'
+      );
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        alert(
+          'Could not locate you unfortunately. Please enter an address manually'
+        );
+      }
+    );
+  }
+  findAddressHandler(event) {
+    event.preventDefault();
+  }
+}
+
+new PlaceFinder();
